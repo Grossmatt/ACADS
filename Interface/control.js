f0db14294fd437911303db16070cfa4f0964fa14
Grossmatt <matthewgrossweiler@yahoo.com>
2020-11-11 22:46:22 -0600

    updates

--- Interface/control.js
+++ Interface/control.js
@@ -1,4 +1,31 @@
 var Promise = require('bluebird');
+const i2c = require('i2c-bus');
+const TIVA_ADDR = 0x1d;
+
+
+
+
+
+var motor1P = 11;
+var motor2P = 22;
+var Avg_Power = 44;
+var cycle = 1;
+var iteration = 0;
+
+const passToGlobalM1 = motor => {
+	motor1P = motor;
+	return;
+};
+
+const passToGlobalM2 = motor => {
+	motor2P = motor;
+	return;
+};
+
+const passToGlobalAP = motor => {
+	Avg_Power = motor;
+	return;
+};
 
 var promiseWhile = function(condition, action) {
     var resolver = Promise.defer();
@@ -16,10 +43,6 @@ var promiseWhile = function(condition, action) {
 };
 
 
-// And below is a sample usage of this promiseWhile function
-var sum = 0,
-    stop = 10;
-
 promiseWhile(function() {
     // Condition for stopping
    return 1;
@@ -28,30 +51,86 @@ promiseWhile(function() {
     return new Promise(function(resolve, reject) {
         // Arbitrary 250ms async method to simulate async process
         setTimeout(function() {
-            sum++;
-            // Print out the sum thus far to show progress
-            //console.log(sum);
+	    if (cycle == 1)
+		{
+			i2c.openPromisified(1).
+				then(i2c1 => i2c1.readByte(TIVA_ADDR, cycle).
+				then(motor => passToGlobalM1(motor)).
+				then(_ => i2c1.close())).
+				catch(console.log);
+				console.log(iteration);
+				console.log("at 1");
+				console.log(motor1P);
+				console.log("");
+		}
+	    else if (cycle == 2)
+		{
+			i2c.openPromisified(1).
+				then(i2c1 => i2c1.readByte(TIVA_ADDR, cycle).
+				then(motor => passToGlobalM2(motor)).
+				then(_ => i2c1.close())).
+				catch(console.log);
+				console.log(iteration);
+				console.log("at 2");
+				console.log(motor2P);
+				console.log("");
+		}
+	    else if (cycle == 3)
+		{
+			i2c.openPromisified(1).
+				then(i2c1 => i2c1.readByte(TIVA_ADDR, cycle).
+				then(motor => passToGlobalAP(motor)).
+				then(_ => i2c1.close())).
+				catch(console.log);
+				console.log(iteration);
+				console.log("at 3");
+				console.log(Avg_Power);
+				console.log("");
+		}
+	    if (cycle > 2)
+		{
+			cycle = 1;
+	 	}
+	    else
+		{
+			cycle = cycle + 1;
+		}
+		iteration = iteration + 1;
             resolve();
-        }, 25);
+
+        }, 5000);
     });
 }).then(function() {
     // Notice we can chain it because it's a Promise, this will run after completion of the promiseWhile Promise!
-    console.log("Done");
 });
 
 
-function i2cobject() {
+function i2cAvgPower() {
 var obj = {
-    power: 1,
-    motor1: 15, 
-    motor2: 3,
+    Avg_Power,
+
+
 }
 
 var jobj = JSON.stringify(obj);
-
 return jobj;
-
 }
+
+function i2cMotor1P() {
+var obj = {
+
+	motor1P,
+}
+var jobj = JSON.stringify(obj);
+return jobj;
+}
+
+
+
+
+
+
+
 
 function sendCommand () {
 
@@ -61,4 +140,4 @@ function sendCommand () {
 
 
 
-module.exports =  { i2cobject, sendCommand };
+module.exports =  { i2cAvgPower, i2cMotor1P, sendCommand };
